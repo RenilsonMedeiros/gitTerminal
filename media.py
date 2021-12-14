187190a91ca9324a108fed54de5ff659dc0b3c65
RenilsonMedeiros <renilsonsilva@aluno.uema.br>
2021-12-14 01:20:25 -0300

    Criei um arquivo python

---
+++ media.py
@@ -0,0 +1,8 @@
+n1 = int(input("Entre com a nota: "))
+n2 = int(input("Entre com a nota: "))
+n3 = int(input("Entre com a nota: "))
+
+media = (n1 + n2 + n3)/3
+
+if media >= 7: print("Aluno aprovado com média ", media)
+else: print("Aluno reprovado com média ", media)
